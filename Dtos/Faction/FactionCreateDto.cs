a7bc54a5899d918232e632f4520f1a5e0e4339d3
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-10-28 09:23:34 +0300

    Added DTOs

---
+++ Dtos/Faction/FactionCreateDto.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rpg.Dtos.Faction
+{
+    public record struct FactionCreateDto(string Name);
+}
