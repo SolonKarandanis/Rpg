43c25988f207b0d1b53e0cea412113d7505c86ef
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-10-28 11:57:29 +0300

    Small Changes

--- Models/User.cs
+++ Models/User.cs
@@ -11,6 +11,9 @@ namespace rpg.Models
         public string Username { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; } = new byte[0];
         public byte[] PasswordSalt { get; set; } = new byte[0];
+        public string FirstName { get; set; }= string.Empty;
+        public string LastName { get; set; }= string.Empty;
+        public string Email { get; set; }= string.Empty;
         public List<Character>? Characters { get; set; }
     }
 }
