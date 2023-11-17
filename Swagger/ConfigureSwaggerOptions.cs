1edff27237ae4b44f2dffc14d3dcddf6eb23db59
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-11-17 07:22:33 +0200

    Swagger config

---
+++ Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace rpg.Swagger
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        public void Configure(SwaggerGenOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
